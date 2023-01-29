8fc45e1f37f704de6b429a18029ff08d50c4f816
schiufreitas <schiufreitas@gmail.com>
2023-01-29 17:31:45 -0300

    setup do projeto

---
+++ src/main/java/ContaCorrente/Api/ApiContaCorrenteApplication.java
@@ -0,0 +1,14 @@
+package ContaCorrente.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContaCorrenteApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApiContaCorrenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ContaCorrente/Api/ApiContaCorrenteApplicationTests.java
@@ -0,0 +1,13 @@
+package ContaCorrente.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiContaCorrenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
